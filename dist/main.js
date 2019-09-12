71694b2ebd8a779ef0f11b03e70620a10750e8d9
Angel <atalavera86@gmail.com>
2019-09-12 23:09:34 +0200

    Middlewares implementation

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("express"));
+const utils_1 = require("./utils");
+const index_1 = __importDefault(require("./middleware/index"));
 const router = express_1.default();
+utils_1.applyMiddleware(index_1.default, router);
 const { PORT = 3000 } = process.env;
 const server = http_1.default.createServer(router);
 server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`));
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import http from "http";
-import express from "express";
+import http from 'http';
+import express from 'express';
+import { applyMiddleware } from './utils';
+import middleware from './middleware/index';
 
 const router = express();
+applyMiddleware(middleware, router);
+
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
 
-server.listen(PORT, () =>
-  console.log(`Server is running http://localhost:${PORT}...`)
-);
+server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`));
